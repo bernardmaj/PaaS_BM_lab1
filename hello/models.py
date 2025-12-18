f6cd3c1ac3870b9b98a9fbacf32601b6efc971f5
Bernard Maj <bermaj@gmail.com>
2025-12-18 08:55:08 +0100

    Add DB visit counter

--- hello/models.py
+++ hello/models.py
@@ -5,3 +5,7 @@ from django.db import models
 
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Visit(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
-from .models import Greeting
+from .models import Greeting, Visit  #<-dodanie Visit
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    # Zapis każdej wizyty w bazie
+    Visit.objects.create()         #<-dodanie linii
+    visits = Visit.objects.count()  #<-dodanie linii
+    return render(request, "index.html", {"visits": visits})  #<-dodanie {.....}
 
 
 def db(request):
